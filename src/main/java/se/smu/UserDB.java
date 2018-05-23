d6b5f09f516bda5c4add5801181db37729db28b0
handongkim <aggsae@gmail.com>
2018-05-23 18:57:47 +0900

    #1206 유저 데이터베이스 클래스 구현
    DB로 직접 접속 후 Table로 접속
    불필요한 java.util import문 삭제

--- src/main/java/se/smu/UserDB.java
+++ src/main/java/se/smu/UserDB.java
@@ -1,14 +1,16 @@
 /**
  * title : UserDB.java
  * author : 김한동 (aggsae@gmail.com)
- * version : 0.0.1.
+ * version : 1.0.1.
  * since : 2018 - 05 - 07
  * brief : UserDB 클래스
  * -----------------------------------
  * history
- *   author  version     date        brief
- *   김한동       0.0.0.   2018-05-18    초안 작성
- *   김한동       0.0.1.   2018-05-18    오타 수정
+ *   author  version     date                           brief
+ *   김한동       0.0.0.   2018-05-18                       초안 작성
+ *   김한동       0.0.1.   2018-05-18                       오타 수정
+ *   김한동       1.0.0.   2018-05-22             DBConn USE UserDB String 값 대체
+ *   김한동       1.0.1.   2018-05-23      DB 직접 접속 후 Table로 접근, 불필요한 스캐너 import 삭제
  * -----------------------------------
  */
 
@@ -16,66 +18,24 @@ package se.smu;
 
 import se.smu.*;
 import java.sql.*;
-import java.util.Scanner;
 
 public class UserDB {
-	void UserTable(String id, String pw, String email) {
+	public static void main(String[] args) {
 		try {
-				// 기본 변수 선언
-				int nUllcheck = 0;
-				String dAtabase;
-				Scanner iN = new Scanner(System.in);
-				Connection cOnn = null;
-				String sQl;
-				Statement st = null;
-				PreparedStatement pst = null;
-				ResultSet rs = null;
-		
-				// DB 연결
-				Class.forName(DBConn.forName);
-				cOnn = DriverManager.getConnection(DBConn.URL, DBConn.ID, DBConn.PW);
-				
-				st = cOnn.createStatement();
-				rs = st.executeQuery("SHOW DATABASES");
-				
-				// DB에 UserDB가 존재하는지 확인
-				while(rs.next()) {
-					dAtabase = rs.getString("USERDB");
-					if(dAtabase.equals("UserDB"))
-						nUllcheck = 1;
-				}
-				
-				// DB가 존재하지 않을 경우 DB 생성
-				if(nUllcheck == 0) {
-					sQl = "CREATE DATABASE UserDB";
-					st.execute(sQl);
-					sQl = "USE UserDB";
-					st.execute(sQl);
-					sQl = "CREATE TABLE UserData (id char(20), pw char(30), email char(35))";
-					st.execute(sQl);
-				}
-				
-				// DB가 존재할 경우 DB에 입력
-				else {
-					sQl = "USE UserDB";
-					sQl = "INSERT INTO UserData (id char(20), pw char(30), email char(35))";
-					pst = cOnn.prepareStatement(sQl);
-					rs = pst.executeQuery("SELECT * FROM UserData");
-				}
-				
-				pst.setString(1, id);
-				pst.setString(2, pw);
-				pst.setString(3, email);
-				pst.executeUpdate();
-				
-				rs.close();
-				pst.close();
+			//Table 접속 변수 선언
+			Connection cOnn = null;
+			Statement st = null;
+			ResultSet rs = null;
+			
+			//Table 접속
+			Class.forName(DBConn.forName);
+			cOnn = DriverManager.getConnection(DBConn.URL, DBConn.ID, DBConn.PW);
+					
+			st = cOnn.createStatement();
+			rs = st.executeQuery("SELECT * FROM UserDB");
+			rs.close();
 		} catch(ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static void main(String[] args) {
-		new UserDB();
 	}
 }
