9c018fff88bb603e334696ae6b3ef1e9471323a5
handongkim <aggsae@gmail.com>
2018-05-24 15:50:24 +0900

    #1206 유저 데이터베이스 클래스 구현
    Table 접근방식일때 발생하는 오류 해결
    삽입기능할 수 있도록 추가

--- src/main/java/se/smu/UserDB.java
+++ src/main/java/se/smu/UserDB.java
@@ -1,7 +1,7 @@
 /**
  * title : UserDB.java
  * author : 김한동 (aggsae@gmail.com)
- * version : 1.0.1.
+ * version : 2.0.0.
  * since : 2018 - 05 - 07
  * brief : UserDB 클래스
  * -----------------------------------
@@ -11,6 +11,7 @@
  *   김한동       0.0.1.   2018-05-18                       오타 수정
  *   김한동       1.0.0.   2018-05-22             DBConn USE UserDB String 값 대체
  *   김한동       1.0.1.   2018-05-23      DB 직접 접속 후 Table로 접근, 불필요한 스캐너 import 삭제
+ *   김한동       2.0.0.   2018-05-24          Table 접근 시 발생하는 문제 해결_원본으로 복구
  * -----------------------------------
  */
 
@@ -20,22 +21,60 @@ import se.smu.*;
 import java.sql.*;
 
 public class UserDB {
-	public static void main(String[] args) {
+	void UserData(String ID, String Password, String Email) {
 		try {
-			//Table 접속 변수 선언
+			int checkNull = 0;
+			String dataBase;
+			String sQl;
 			Connection cOnn = null;
 			Statement st = null;
+			PreparedStatement pst = null;
 			ResultSet rs = null;
 			
-			//Table 접속
 			Class.forName(DBConn.forName);
 			cOnn = DriverManager.getConnection(DBConn.URL, DBConn.ID, DBConn.PW);
-					
+			
 			st = cOnn.createStatement();
-			rs = st.executeQuery("SELECT * FROM UserDB");
+			rs = st.executeQuery("SHOW DATABASES");
+			
+			while(rs.next()) {
+				dataBase = rs.getString("Database");
+				if(dataBase.equals("userdb"))
+					checkNull = 1;
+			}
+			
+			if(checkNull == 0) {
+				sQl = "CREATE DATABASE UserDB";
+				st.execute(sQl);
+				sQl = "USE UserDB";
+				st.execute(sQl);
+				sQl = "CREATE TABLE UserData(ID char(20) NOT NULL PRIMARY KEY, Password char(30) NOT NULL, Email char(30) NOT NULL)";
+				st.execute(sQl);
+			}
+			
+			else {
+				sQl = "USE UserDB";
+				st.execute(sQl);
+				
+				sQl = "INSERT INTO UserData(ID, Password, Email) VALUES(?, ?, ?)";
+				pst = cOnn.prepareStatement(sQl);
+				
+				pst.setString(1, ID);
+				pst.setString(2, Password);
+				pst.setString(3, Email);
+				pst.executeUpdate();
+				
+				rs = pst.executeQuery("SELECT * FROM UserData");
+			}
+			
 			rs.close();
-		} catch(ClassNotFoundException | SQLException e) {
+			pst.close();
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static void main(String[] args) {
+		new UserDB();
 	}
 }
