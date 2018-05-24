34dace2767c3e4322c9aece99b0d19d694f41b49
handongkim <aggsae@gmail.com>
2018-05-24 15:49:33 +0900

    #1207 데이터베이스 연결 클래스 구현
    Insert 쿼리 실행 시 에러 발생 수정

--- src/main/java/se/smu/DBConn.java
+++ src/main/java/se/smu/DBConn.java
@@ -1,15 +1,16 @@
 /**
  * title : DBConn.java
  * author : 김한동 (aggsae@gmail.com)
- * version : 1.0.0.
+ * version : 2.0.0.
  * since : 2018 - 05 - 07
- * brief : DB 연동 String
+ * brief : DB 연동 클래스
  * -----------------------------------
  * history
- *   author  version     date              brief
- *   김한동       0.0.1.   2018-05-15          초안 작성
- *   김한동       0.0.2.   2018-05-16     데이터베이스 연결자 통합
- *   김한동       1.0.0.   2018-05-23        DB에 직접 접속
+ *   author  version     date                      brief
+ *   김한동       0.0.1.   2018-05-15                  초안 작성
+ *   김한동       0.0.2.   2018-05-16             데이터베이스 연결자 통합
+ *   김한동       1.0.0.   2018-05-23                DB에 직접 접속
+ *   김한동       2.0.0.   2018-05-24    DB 접속 시 Insert문에 오류 발생하여 URL 수정
  * -----------------------------------
  */
 
@@ -18,7 +19,9 @@ package se.smu;
 public class DBConn {
 	//접속할 DB 정보
 	public static String forName = "com.mysql.jdbc.Driver";
-	public static String URL = "jdbc:mysql://localhost:3306/test";
+	public static String URL = "jdbc:mysql://localhost:3306/mysql?useSSL=false";
+	//test DB에 직접 접속하여 table로 데이터를 다루는 코드
+	//public static String URL = "jdbc:mysql://localhost:3306/test";
 	
 	//DB 로그인 정보
 	public static String ID = "root";
