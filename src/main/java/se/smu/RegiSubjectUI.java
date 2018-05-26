c29d3213b863cdd9d2a15b1fabd54e44daf22df9
handongkim <aggsae@gmail.com>
2018-05-26 10:58:29 +0900

    #1260 과목 등록 및 수정 화면 메소드 구현
    과목 등록 메소드 설정
    버튼기능 활성화, 중복확인 버튼 및 기능 추가
    입력값들에 대한 예외처리
    중복과목 UI 버튼 활성화

--- src/main/java/se/smu/RegiSubjectUI.java
+++ src/main/java/se/smu/RegiSubjectUI.java
@@ -1,7 +1,7 @@
 /**
  * title : RegiSubjectUI.java
  * author : ±èÇÑµ¿ (aggsae@gmail.com)
- * version : 1.0.0.
+ * version : 2.1.0.
  * since : 2018 - 05 - 07
  * brief : °ú¸ñ Ç×¸ñ µî·Ï UI
  * -----------------------------------
@@ -9,12 +9,15 @@
  *   author  version     date                          brief
  *   ¾Èµ¿ÁÖ       0.0.0.   2018-05-22                      ÃÊ¾È ÀÛ¼º
  *   ±èÇÑµ¿       1.0.0.   2018-05-25                  ÆÐÅ°Áö Ãß°¡, ÁÖ¼® ÀÛ¼º
+ *   ±èÇÑµ¿       2.0.0.   2018-05-26           ¹öÆ° ±â´É È°¼ºÈ­, Áßº¹È®ÀÎ ¹öÆ° ¹× ±â´É Ãß°¡
+ *   ±èÇÑµ¿       2.1.0.   2018-05-26                 ÀÔ·Â°ªµé¿¡ ´ëÇÑ ¿¹¿ÜÃ³¸®
  * -----------------------------------
  */
 
 package se.smu;
 
 import se.smu.*;
+import java.sql.*;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Color;
@@ -32,15 +35,44 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JLabel;
 
 public class RegiSubjectUI extends JFrame {
+	
+	static String Subject;
+	static String Professor;
+	static String subjectYear;
+	static String subjectSem;
+	static String subjectDate;
+	static String subjectStart;
+	static String subjectEnd;
+	static String room;
+	
+	static String InputSubject;
+	static String InputProfessor;
+	static String InputSubjectYear;
+	static String InputSubjectSem;
+	static String InputSubjectDate;
+	static String InputSubjectStart;
+	static String InputSubjectEnd;
+	static String InputRoom;
 
+	/*null ÀÔ·Â È®ÀÎ º¯¼ö
+	static Boolean subjectNullCheck = false;
+	static Boolean professorNullCheck = false;
+	static Boolean subjectYearNullCheck = false;
+	static Boolean subjectDateNullCheck = false;
+	static Boolean subjectStartNullCheck = false;
+	static Boolean subjectEndNullCheck = false;
+	static Boolean roomNullCheck = false;
+	*/
+	
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
+	private JTextField subjectTextField;
+	private JTextField professorTextField;
+	private JTextField subjectYearTextField;
+	private JTextField subjectSemTextField;
+	private JTextField subjectDateTextField;
+	private JTextField subjectStartTextField;
+	private JTextField subjectEndTextField;
+	private JTextField roomTextField;
 
 	public RegiSubjectUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -51,131 +83,141 @@ public class RegiSubjectUI extends JFrame {
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 		
-		JButton btnNewButton = new JButton("\uB4F1\uB85D");
-		btnNewButton.setBackground(Color.DARK_GRAY);
-		btnNewButton.setForeground(Color.WHITE);
-		btnNewButton.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton.setBounds(430, 210, 100, 30);
-		contentPane.add(btnNewButton);
+		JButton checkSubjectButton = new JButton("\uC911\uBCF5\uD655\uC778");
+		checkSubjectButton.setForeground(Color.WHITE);
+		checkSubjectButton.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		checkSubjectButton.setBackground(Color.DARK_GRAY);
+		checkSubjectButton.setBounds(430, 90, 100, 30);
+		contentPane.add(checkSubjectButton);
 		
-		JButton button = new JButton("\uCDE8\uC18C");
-		button.setForeground(Color.WHITE);
-		button.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		button.setBackground(Color.DARK_GRAY);
-		button.setBounds(430, 280, 100, 30);
-		contentPane.add(button);
+		JButton regiButton = new JButton("\uB4F1\uB85D");
+		regiButton.setBackground(Color.DARK_GRAY);
+		regiButton.setForeground(Color.WHITE);
+		regiButton.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		regiButton.setBounds(430, 210, 100, 30);
+		contentPane.add(regiButton);
 		
-		JTextPane textPane = new JTextPane();
-		textPane.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		textPane.setText("\uACFC\uBAA9\uBA85");
-		textPane.setBounds(50, 90, 80, 30);
-		contentPane.add(textPane);
+		JButton cancelButton = new JButton("\uCDE8\uC18C");
+		cancelButton.setForeground(Color.WHITE);
+		cancelButton.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		cancelButton.setBackground(Color.DARK_GRAY);
+		cancelButton.setBounds(430, 280, 100, 30);
+		contentPane.add(cancelButton);
 		
-		JTextPane textPane_1 = new JTextPane();
-		textPane_1.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		textPane_1.setText("\uB2F4\uB2F9\uAD50\uC218");
-		textPane_1.setBounds(50, 130, 87, 27);
-		contentPane.add(textPane_1);
+		JLabel subjectLabel = new JLabel();
+		subjectLabel.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		subjectLabel.setText("\uACFC\uBAA9\uBA85");
+		subjectLabel.setBounds(50, 90, 80, 30);
+		contentPane.add(subjectLabel);
 		
-		JTextPane textPane_2 = new JTextPane();
-		textPane_2.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		textPane_2.setText("\uAC15\uC758\uC2DC\uAC04");
-		textPane_2.setBounds(50, 170, 87, 27);
-		contentPane.add(textPane_2);
+		JLabel professorLabel = new JLabel();
+		professorLabel.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		professorLabel.setText("\uB2F4\uB2F9\uAD50\uC218");
+		professorLabel.setBounds(50, 130, 87, 27);
+		contentPane.add(professorLabel);
 		
-		JTextPane textPane_3 = new JTextPane();
-		textPane_3.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		textPane_3.setText("\uAC15\uC758\uC2DC\uAC04");
-		textPane_3.setBounds(50, 210, 87, 27);
-		contentPane.add(textPane_3);
+		JLabel subjectYearSem = new JLabel();
+		subjectYearSem.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		subjectYearSem.setText("\uAC15\uC758\uB144\uB3C4");
+		subjectYearSem.setBounds(50, 170, 87, 27);
+		contentPane.add(subjectYearSem);
 		
-		JTextPane textPane_4 = new JTextPane();
-		textPane_4.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		textPane_4.setText("\uAC15\uC758\uC2E4");
-		textPane_4.setBounds(50, 250, 87, 27);
-		contentPane.add(textPane_4);
+		JLabel subjectTime = new JLabel();
+		subjectTime.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		subjectTime.setText("\uAC15\uC758\uC2DC\uAC04");
+		subjectTime.setBounds(50, 210, 87, 27);
+		contentPane.add(subjectTime);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		textField.setBounds(150, 90, 184, 24);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		JLabel subjectRoom = new JLabel();
+		subjectRoom.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		subjectRoom.setText("\uAC15\uC758\uC2E4");
+		subjectRoom.setBounds(50, 250, 87, 27);
+		contentPane.add(subjectRoom);
 		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		textField_1.setColumns(10);
-		textField_1.setBounds(150, 130, 184, 24);
-		contentPane.add(textField_1);
+		subjectTextField = new JTextField();
+		subjectTextField.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		subjectTextField.setBounds(150, 90, 184, 24);
+		contentPane.add(subjectTextField);
+		subjectTextField.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		textField_2.setColumns(10);
-		textField_2.setBounds(150, 170, 56, 24);
-		contentPane.add(textField_2);
+		professorTextField = new JTextField();
+		professorTextField.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		professorTextField.setColumns(10);
+		professorTextField.setBounds(150, 130, 184, 24);
+		contentPane.add(professorTextField);
 		
-		JTextPane textPane_5 = new JTextPane();
+		subjectYearTextField = new JTextField();
+		subjectYearTextField.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		subjectYearTextField.setColumns(10);
+		subjectYearTextField.setBounds(150, 170, 56, 24);
+		contentPane.add(subjectYearTextField);
+		
+		JLabel textPane_5 = new JLabel();
 		textPane_5.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
 		textPane_5.setText("\uB144\uB3C4");
 		textPane_5.setBounds(211, 170, 45, 24);
 		contentPane.add(textPane_5);
 		
-		textField_3 = new JTextField();
-		textField_3.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		textField_3.setColumns(10);
-		textField_3.setBounds(260, 170, 35, 24);
-		contentPane.add(textField_3);
+		subjectSemTextField = new JTextField();
+		subjectSemTextField.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		subjectSemTextField.setColumns(10);
+		subjectSemTextField.setBounds(260, 170, 35, 24);
+		contentPane.add(subjectSemTextField);
 		
-		JTextPane textPane_6 = new JTextPane();
+		JLabel textPane_6 = new JLabel();
 		textPane_6.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
 		textPane_6.setText("\uD559\uAE30");
 		textPane_6.setBounds(300, 170, 45, 24);
 		contentPane.add(textPane_6);
 		
-		textField_4 = new JTextField();
-		textField_4.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		textField_4.setColumns(10);
-		textField_4.setBounds(232, 210, 35, 30);
-		contentPane.add(textField_4);
+		subjectStartTextField = new JTextField();
+		subjectStartTextField.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		subjectStartTextField.setColumns(10);
+		subjectStartTextField.setBounds(232, 210, 35, 30);
+		contentPane.add(subjectStartTextField);
 		
-		JTextPane textPane_7 = new JTextPane();
+		JLabel textPane_7 = new JLabel();
 		textPane_7.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
 		textPane_7.setText("\uC2DC");
 		textPane_7.setBounds(269, 210, 24, 24);
 		contentPane.add(textPane_7);
 		
-		JTextPane textPane_8 = new JTextPane();
+		JLabel textPane_8 = new JLabel();
 		textPane_8.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
 		textPane_8.setText("\uC2DC");
 		textPane_8.setBounds(349, 210, 34, 24);
 		contentPane.add(textPane_8);
 		
-		textField_5 = new JTextField();
-		textField_5.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		textField_5.setColumns(10);
-		textField_5.setBounds(314, 210, 35, 30);
-		contentPane.add(textField_5);
+		subjectEndTextField = new JTextField();
+		subjectEndTextField.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		subjectEndTextField.setColumns(10);
+		subjectEndTextField.setBounds(314, 210, 35, 30);
+		contentPane.add(subjectEndTextField);
 		
-		JTextPane textPane_9 = new JTextPane();
+		JLabel textPane_9 = new JLabel();
 		textPane_9.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
 		textPane_9.setText("~");
 		textPane_9.setBounds(291, 210, 26, 24);
 		contentPane.add(textPane_9);
 		
-		textField_6 = new JTextField();
-		textField_6.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
-		textField_6.setColumns(10);
-		textField_6.setBounds(150, 250, 56, 24);
-		contentPane.add(textField_6);
+		roomTextField = new JTextField();
+		roomTextField.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		roomTextField.setColumns(10);
+		roomTextField.setBounds(150, 250, 56, 24);
+		contentPane.add(roomTextField);
 		
+		subjectDateTextField = new JTextField();
+		subjectDateTextField.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
+		subjectDateTextField.setBounds(150, 210, 80, 30);
+		contentPane.add(subjectDateTextField);
+		
+		/*
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"\uC6D4\uC694\uC77C", "\uD654\uC694\uC77C", "\uC218\uC694\uC77C", "\uBAA9\uC694\uC77C", "\uAE08\uC694\uC77C", "\uD1A0\uC694\uC77C", "\uC77C\uC694\uC77C"}));
 		comboBox.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 15));
 		comboBox.setBounds(150, 210, 80, 30);
 		contentPane.add(comboBox);
+		*/
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.DARK_GRAY);
@@ -186,6 +228,96 @@ public class RegiSubjectUI extends JFrame {
 		label.setForeground(Color.WHITE);
 		label.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 35));
 		panel.add(label);
+		
+		checkSubjectButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Connection cOnn = null;
+					String sQl;
+					Statement st = null;
+					ResultSet rs = null;
+					
+					String subjectCheck = null;
+					InputSubject = subjectTextField.getText();
+					
+					Class.forName(DBConn.forName);
+					cOnn = DriverManager.getConnection(DBConn.URL, DBConn.ID, DBConn.PW);
+					st = cOnn.createStatement();
+					sQl = "USE SubjectDB";
+					st.executeQuery(sQl);
+					rs = st.executeQuery("SELECT Subject FROM SubjectData WHERE Subject = '" + InputSubject + "'");
+					
+					while(rs.next())
+						subjectCheck = rs.getString("Subject");
+					
+					if(InputSubject.length() == 0) {
+						ReEnterRequest noNull = new ReEnterRequest();
+						noNull.setVisible(true);
+					}
+					
+					else {
+						if(InputSubject.equals(subjectCheck)) {
+							NameOverlap usedSubject = new NameOverlap();
+							usedSubject.setVisible(true);
+							dispose();
+						}
+						
+						else {
+							System.out.println("µî·Ï°¡´ÉÇÑ °ú¸ñÀÔ´Ï´Ù.");
+							//¸Þ¼¼Áö UI ±¸Ãà ÈÄ Ãß°¡
+						}
+					}
+												
+				} catch(ClassNotFoundException | SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		regiButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				InputSubject = subjectTextField.getText();
+				InputProfessor = professorTextField.getText();
+				InputSubjectYear = subjectYearTextField.getText();
+				InputSubjectSem = subjectSemTextField.getText();
+				InputSubjectDate = subjectDateTextField.getText();
+				InputSubjectStart = subjectStartTextField.getText();
+				InputSubjectEnd = subjectEndTextField.getText();
+				InputRoom = roomTextField.getText();
+				
+				//ÀÔ·Â°ª Áß null°ªÀÌ ¾øÀ» °æ¿ì °ú¸ñÀ» µ¥ÀÌÅÍº£ÀÌ½º¿¡ µî·ÏÇÏ°í ¸ÞÀÎÈ­¸éÀ¸·Î µ¹¾Æ°¨
+				if(InputSubject.length() == 0 | InputProfessor.length() == 0 | InputSubjectYear.length() == 0 | InputSubjectSem.length() == 0 | InputSubjectStart.length() == 0 | InputSubjectEnd.length() == 0 | InputRoom.length() == 0 ) {
+					ReEnterRequest noNull = new ReEnterRequest();
+					noNull.setVisible(true);
+				}
+				else {
+					// ÇÐ±â°¡ 1~4, ½Ã°£ÀÌ 0~24½Ã ¹üÀ§¾È¿¡ ¾øÀ» °æ¿ì, ½ÃÀÛ½Ã°£ÀÌ Á¾·á½Ã°£º¸´Ù ºü¸¥ °æ¿ì ¿¹¿ÜÃ³¸®
+					if((Integer.parseInt(InputSubjectSem) < 1 || Integer.parseInt(InputSubjectSem) > 4) ||
+							(Integer.parseInt(InputSubjectStart) < 0 || Integer.parseInt(InputSubjectStart) > 24) ||
+							(Integer.parseInt(InputSubjectEnd) < 0 || Integer.parseInt(InputSubjectEnd) > 24) ||
+							(Integer.parseInt(InputSubjectStart) > Integer.parseInt(InputSubjectEnd))) {
+						System.out.println("ÇÐ±â´Â 1~4ÇÐ±â, ½Ã°£Àº 0~24½Ã, ½ÃÀÛ½Ã°£Àº Á¾·á½Ã°£º¸´Ù ÀÛ°Ô ¼³Á¤ÇÏ½Ê½Ã¿À.");
+						//¸Þ¼¼Áö UI ±¸Çö ÈÄ Ãß°¡
+					}
+					else {
+						SubjectDB subjectDB = new SubjectDB();
+						subjectDB.SubjectData(InputSubject, InputProfessor, InputSubjectYear, InputSubjectSem, InputSubjectDate, InputSubjectStart, InputSubjectEnd, InputRoom);
+					
+						MainUI backToMain = new MainUI();
+						backToMain.setVisible(true);
+						dispose();
+					}
+				}
+			}
+		});
+		
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MainUI backToMain = new MainUI();
+				backToMain.setVisible(true);
+				dispose();
+			}
+		});
 		
 	}
 	
