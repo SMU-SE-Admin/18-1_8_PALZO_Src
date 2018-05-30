4919c2db6816fae1aa718350762529b137134baa
handongkim <aggsae@gmail.com>
2018-05-30 09:16:25 +0900

    #1207 데이터베이스 연결 클래스 구현
    최종본 업로드

--- src/main/java/se/smu/DBConn.java
+++ src/main/java/se/smu/DBConn.java
@@ -1,7 +1,7 @@
 /**
  * title : DBConn.java
  * author : 김한동 (aggsae@gmail.com)
- * version : 2.0.0.
+ * version : 3.0.0.
  * since : 2018 - 05 - 07
  * brief : DB 연동 클래스
  * -----------------------------------
@@ -11,19 +11,19 @@
  *   김한동       0.0.2.   2018-05-16             데이터베이스 연결자 통합
  *   김한동       1.0.0.   2018-05-23                DB에 직접 접속
  *   김한동       2.0.0.   2018-05-24    DB 접속 시 Insert문에 오류 발생하여 URL 수정
+ *   김한동       3.0.0.   2018-05-30                 최종본 작성
  * -----------------------------------
  */
 
 package se.smu;
 
 public class DBConn {
-	//접속할 DB 정보
 	public static String forName = "com.mysql.jdbc.Driver";
 	public static String URL = "jdbc:mysql://localhost:3306/mysql?useSSL=false";
+	
 	//test DB에 직접 접속하여 table로 데이터를 다루는 코드
 	//public static String URL = "jdbc:mysql://localhost:3306/test";
-	
-	//DB 로그인 정보
+
 	public static String ID = "root";
 	public static String PW = "1q2w3e4r";
 }
